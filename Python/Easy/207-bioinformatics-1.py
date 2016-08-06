7655be14c865c2095060dd36efca1ba970b985f3
Larissa Lu <Larissa Lu>
2016-08-06 14:13:53 -0400

    rewrote program to use replace function

--- Python/Easy/207-bioinformatics-1.py
+++ Python/Easy/207-bioinformatics-1.py
@@ -1,22 +1,20 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Aug 06 11:40:51 2016
-
+Updated last on Sat Aug 06
 /u/CirclingNeptune
+
+I originally wrote this using if statements, but then rewrote it using the
+replace() function to make the program shorter
 """
 
-origDNA = raw_input('Input DNA')
-compDNA = ''
+origDNA = raw_input('Input DNA: ')
 
-for base in origDNA:
-    if base == 'A':
-        compDNA += 'T'
-    if base == 'T':
-        compDNA += 'A'
-    if base == 'G':
-        compDNA += 'C'
-    if base == 'C':
-        compDNA += 'G'
+compDNA = origDNA.replace('A', '-')
+compDNA = compDNA.replace('T', 'A')
+compDNA = compDNA.replace('-', 'T')
+compDNA = compDNA.replace('G', '-')
+compDNA = compDNA.replace('C', 'G')
+compDNA = compDNA.replace('-', 'C')
 
 print origDNA
-print ' '.join(compDNA)
+print compDNA
